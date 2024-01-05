3fcc3834975f166b7fa0c60695a315dd173bab6d
dongcheon1020 <dongcheon1020@gmail.com>
2024-01-05 13:23:03 +0900

    feat: intersectionobserver 기능 추가

--- textshow.js
+++ textshow.js
@@ -1,28 +1,22 @@
-let observer = new IntersectionObserver((e) => {
-  e.forEach((text) => {
-    if (text.isIntersecting) {
-      text.target.style.opacity = 1;
-      text.target.style.transform = "translateY(0)";
-    } else {
-      text.target.style.opacity = 0;
-      text.target.style.transform = "translateY(50px)";
-    }
-    text.intersectionRatio(0.5);
-  });
-});
-let show = document.querySelectorAll(".show");
+let observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.intersectionRatio === 1) {
+        entry.target.style.opacity = 1;
+        entry.target.style.transform = "translateY(0)";
+        entry.target.style.transition = "opacity 0.5s, transform 0.5s";
+      } else {
+        entry.target.style.opacity = 0;
+        entry.target.style.transform = "translateY(30px)";
+        entry.target.style.transition = "opacity 0.5s, transform 0.5s";
+      }
+    });
+  },
+  { threshold: 1 }
+);
 
-observer.observe(show[0]);
-observer.observe(show[1]);
-observer.observe(show[2]);
-observer.observe(show[3]);
-observer.observe(show[4]);
-observer.observe(show[5]);
-observer.observe(show[6]);
-observer.observe(show[7]);
-observer.observe(show[8]);
-observer.observe(show[9]);
-observer.observe(show[10]);
-observer.observe(show[11]);
-observer.observe(show[12]);
-observer.observe(show[13]);
+let show = document.querySelectorAll(".evtext");
+
+show.forEach((element) => {
+  observer.observe(element);
+});
